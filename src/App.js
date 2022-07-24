69fb3e3ead6e7f38e0121a3cf4da7356f179d56d
jnthnpstl <jnthnpstl@gmail.com>
2022-07-24 08:49:25 +0800

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import Item from "./components/Item";
 
 function App() {
   const [items, setItems] = useState([]);
@@ -8,6 +7,7 @@ function App() {
   const [count, setCount] = useState(0);
   const [currentItem, setCurrentItem] = useState(0);
   const [showResult, setShowResult] = useState(false);
+  const [score, setScore] = useState(0);
   const [input, setInput] = useState({
     question: "",
     choice1: "",
@@ -16,14 +16,21 @@ function App() {
     choice4: "",
   });
 
-  const [correct, setCorrect] = useState({
-    correct1: false,
-    correct2: false,
-    correct3: false,
-    correct4: false,
-  });
+  const addItem = (e) => {
+    e.preventDefault();
 
-  const addItem = () => {
+    let radios = document.getElementsByName("correct");
+    let tempArray = Array(4);
+    let index = 0;
+
+    for (let radio of radios) {
+      tempArray[index] = radio.checked;
+      if (radio.checked) {
+        radio.checked = !radio.checked;
+      }
+      index++;
+    }
+
     setItems(() => [
       ...items,
       {
@@ -31,19 +38,19 @@ function App() {
         choices: [
           {
             choice: input.choice1,
-            isCorrect: correct.correct1,
+            isCorrect: tempArray[0],
           },
           {
             choice: input.choice2,
-            isCorrect: correct.correct2,
+            isCorrect: tempArray[1],
           },
           {
             choice: input.choice3,
-            isCorrect: correct.correct3,
+            isCorrect: tempArray[2],
           },
           {
             choice: input.choice4,
-            isCorrect: correct.correct4,
+            isCorrect: tempArray[3],
           },
         ],
       },
@@ -57,29 +64,11 @@ function App() {
       choice4: "",
     });
 
-    console.log(correct);
-
-    setCorrect({
-      correct1: false,
-      correct2: false,
-      correct3: false,
-      correct4: false,
-    });
-
     setCount(count + 1);
   };
 
   const startQuiz = () => {
     setQuizNow(true);
-  };
-
-  const handlCorrectAnswer = (e) => {
-    let temp = JSON.parse(e.target.value);
-    setCorrect({
-      ...correct,
-      [e.target.name]: !temp,
-    });
-    console.log(e.target.defaultChecked);
   };
 
   const handleChange = (e) => {
@@ -89,7 +78,10 @@ function App() {
     });
   };
 
-  const nextItem = () => {
+  const nextItem = (correct) => {
+    if (correct) {
+      setScore(score + 1);
+    }
     if (currentItem >= count - 1) {
       setShowResult(!showResult);
       return;
@@ -97,102 +89,105 @@ function App() {
     setCurrentItem(currentItem + 1);
   };
 
-  const handleCheckBox = () => {
-    console.log("Hello");
-  };
-
   return quizNow ? (
     showResult ? (
       <>
-        <h1>You scored Nothing!</h1>
+        <h1>You scored {score}!</h1>
       </>
     ) : (
       <>
-        <h1>{items[currentItem].question}</h1>
-        {items[currentItem].choices.map((choice) => (
-          <button key={Math.floor(Math.random() * 1000)}>
-            {choice.choice}
-          </button>
-        ))}
-        <button onClick={nextItem}>Next Question</button>
+        <div className="question-wrapper">
+          <div className="question-field">
+            <h1>{items[currentItem].question}</h1>
+          </div>
+          <div className="button-choices">
+            {items[currentItem].choices.map((choice) => (
+              <button
+                onClick={() => nextItem(choice.isCorrect)}
+                key={Math.floor(Math.random() * 1000)}
+              >
+                {choice.choice}
+              </button>
+            ))}
+          </div>
+        </div>
       </>
     )
   ) : (
     <>
-      <form>
-        <input
-          type="text"
-          placeholder={"Question"}
-          value={input.question}
-          onChange={handleChange}
-          name="question"
-        />
-      </form>
-      <form>
-        <input
-          type="checkbox"
-          value={correct.correct1}
-          name="correct1"
-          checked={false}
-          onChange={handlCorrectAnswer}
-        />
-        <input
-          type="text"
-          placeholder="Choices"
-          value={input.choice1}
-          onChange={handleChange}
-          name="choice1"
-        />
-      </form>
+      <div className="form-wrapper">
+        <div className="form-area">
+          <form onSubmit={addItem}>
+            <div className="question">
+              <textarea
+                rows={5}
+                cols={40}
+                type="text"
+                placeholder={"Question"}
+                value={input.question}
+                onChange={handleChange}
+                name="question"
+              />
+            </div>
 
-      <form>
-        <input
-          type="checkbox"
-          value={correct.correct2}
-          name="correct2"
-          onClick={handlCorrectAnswer}
-        />
-        <input
-          type="text"
-          placeholder="Choices"
-          value={input.choice2}
-          onChange={handleChange}
-          name="choice2"
-        />
-      </form>
-      <form>
-        <input
-          type="checkbox"
-          value={correct.correct3}
-          name="correct3"
-          onClick={handlCorrectAnswer}
-        />
-        <input
-          type="text"
-          placeholder="Choices"
-          value={input.choice3}
-          onChange={handleChange}
-          name="choice3"
-        />
-      </form>
-      <form>
-        <input
-          type="checkbox"
-          value={correct.correct4}
-          name="correct4"
-          onClick={handlCorrectAnswer}
-        />
-        <input
-          type="text"
-          placeholder="Choices"
-          value={input.choice4}
-          onChange={handleChange}
-          name="choice4"
-        />
-      </form>
-      <button onClick={addItem}>Add Item</button>
-      <h1>{count > 1 ? <>{count} items </> : <>{count} item</>} </h1>
-      <button onClick={startQuiz}>Start Quiz</button>
+            <div className="choice">
+              <input type="radio" name="correct" id="choice1" />
+              <label htmlFor="choice1">
+                <input
+                  type="text"
+                  placeholder="Choices"
+                  value={input.choice1}
+                  onChange={handleChange}
+                  name="choice1"
+                />
+              </label>
+            </div>
+
+            <div className="choice">
+              <input type="radio" name="correct" id="choice2" />
+              <label htmlFor="choice2">
+                <input
+                  type="text"
+                  placeholder="Choices"
+                  value={input.choice2}
+                  onChange={handleChange}
+                  name="choice2"
+                />
+              </label>
+            </div>
+
+            <div className="choice">
+              <input type="radio" name="correct" id="choice3" />
+              <label htmlFor="choice3">
+                <input
+                  type="text"
+                  placeholder="Choices"
+                  value={input.choice3}
+                  onChange={handleChange}
+                  name="choice3"
+                />
+              </label>
+            </div>
+            <div className="choice">
+              <input type="radio" name="correct" id="choice4" />
+              <label htmlFor="choice4">
+                <input
+                  type="text"
+                  placeholder="Choices"
+                  value={input.choice4}
+                  onChange={handleChange}
+                  name="choice4"
+                />
+              </label>
+            </div>
+            <div className="buttons">
+              <input type={"submit"} value="Add Item"></input>
+              <h1>{count > 1 ? <>{count} items </> : <>{count} item</>} </h1>
+              <button onClick={startQuiz}>Start Quiz</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
